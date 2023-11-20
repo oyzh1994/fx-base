0e8a7b264ab4fd9f1fb3ea8d226cc4c6c1161e2b
oyzh <oyzh.1994@qq.com>
2023-11-20 18:27:01 +0800

    1. 新增部分事件功能 2. 部分细节处理

---
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/event/EventMsgFormatter.java
@@ -0,0 +1,18 @@
+package cn.oyzh.fx.plus.event;
+
+/**
+ * 事件消息格式化器
+ *
+ * @author oyzh
+ * @since 2023/9/18
+ */
+public interface EventMsgFormatter {
+
+    /**
+     * 格式化消息
+     *
+     * @return 格式化后的消息
+     */
+    String formatMsg();
+
+}
