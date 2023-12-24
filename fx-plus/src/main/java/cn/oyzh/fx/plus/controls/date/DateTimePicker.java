4286a166a825008eab778d9391415a0d8fbdc918
oyzh1994 <oyzh.1994@qq.com>
2023-12-24 19:56:06 +0800

    1. 新增日期时间组件

---
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/controls/date/DateTimePicker.java
@@ -0,0 +1,150 @@
+package cn.oyzh.fx.plus.controls.date;
+
+import cn.oyzh.fx.plus.util.ResourceUtil;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Popup;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ResourceBundle;
+
+/**
+ * @author oyzh
+ * @since 2023/12/24
+ */
+public class DateTimePicker extends HBox {
+    private final DateTimeFormatter formatter;
+
+    // DateTime value
+    private ObjectProperty<LocalDateTime> dateTime;
+
+    private Popup popupContainer;
+
+    private DateTimePickerSelect dateTimePickerSelect;
+
+    public Boolean showLocalizedDateTime = false;
+
+    private final TextField textField;
+
+    private final Button button;
+
+    public DateTimePicker() {
+        this.textField = new TextField();
+        this.textField.setDisable(true);
+        this.textField.setFocusTraversable(false);
+        this.textField.setId("dt_text_field");
+        this.getChildren().add(this.textField);
+
+        this.button = new Button();
+        this.button.setId("dt_button");
+        this.button.setGraphic(new ImageView("/fx-plus/img/calendar.png"));
+        this.button.setOnAction(this::handleButtonAction);
+        this.getChildren().add(this.button);
+
+        this.formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
+
+        if (showLocalizedDateTime) {
+            textField.setText(formatter.format(dateTime.get()));
+            this.dateTime = new SimpleObjectProperty<>(LocalDateTime.now());
+        }
+        button.prefHeightProperty().bind(textField.heightProperty());
+    }
+
+    protected void handleButtonAction(ActionEvent event) {
+        if (popupContainer != null && popupContainer.isShowing()) {
+            popupContainer.hide();
+        } else {
+            if (this.popupContainer == null) {
+                this.popupContainer = new Popup();
+                this.dateTimePickerSelect = new DateTimePickerSelect(this);
+                popupContainer.getContent().add(dateTimePickerSelect);
+                popupContainer.autoHideProperty().set(true);
+            }
+            final Window window = button.getScene().getWindow();
+            final double x = window.getX()
+                    + textField.localToScene(0, 0).getX()
+                    + textField.getScene().getX();
+            final double y = window.getY()
+                    + button.localToScene(0, 0).getY()
+                    + button.getScene().getY()
+                    + button.getHeight();
+
+//            popupContainer.setWidth(100);
+//            popupContainer.setHeight(100);
+
+            System.out.println(this.getParent());
+            popupContainer.show(this.getParent(), x, y);
+            dateTimePickerSelect.upDataCalendar(true);
+        }
+    }
+
+    /**
+     * Gets the current LocalDateTime value
+     *
+     * @return The current LocalDateTime value
+     */
+    public ObjectProperty<LocalDateTime> dateTimeProperty() {
+        return dateTime;
+    }
+
+    /**
+     * @Description: setTimeProperty
+     * @Params
+     * @Return
+     * @Author wzy
+     * @Date 2023/4/19 11:11
+     **/
+    public void setTimeProperty(LocalDateTime localDateTime) {
+        this.dateTime = new SimpleObjectProperty<LocalDateTime>(localDateTime);
+        textField.setText(formatter.format(this.dateTime.get()));
+    }
+
+    /**
+     * @Description: clearTimeProperty
+     * @Params
+     * @Return
+     * @Author wzy
+     * @Date 2023/4/19 11:11
+     **/
+    public void clearTimeProperty() {
+        this.dateTime = null;
+        textField.setText("");
+    }
+
+    public void hide() {
+        if (popupContainer.isShowing()) {
+            popupContainer.hide();
+        }
+    }
+
+    public Boolean getShowLocalizedDateTime() {
+        return showLocalizedDateTime;
+    }
+
+    public void setShowLocalizedDateTime(Boolean show) {
+        this.showLocalizedDateTime = show;
+        if (show) {
+            setTimeProperty(LocalDateTime.now());
+        }
+
+    }
+
+    @Override
+    public String getUserAgentStylesheet() {
+        return ResourceUtil.toExternalUrl("/fx-plus/css/date-time-picker.css");
+    }
+}
---
+++ fx-plus/src/test/java/cn/oyzh/fx/plus/test/DateTimePickerMain.java
@@ -0,0 +1,69 @@
+package cn.oyzh.fx.plus.test;
+
+import cn.oyzh.fx.plus.controls.date.DateTimePicker;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.time.LocalDateTime;
+
+public class DateTimePickerMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //初始化一个时间选择器
+        DateTimePicker dateTimePicker = new DateTimePicker();
+
+        //设置选中时间 为 当前时间
+//		dateTimePicker.setTimeProperty( LocalDateTime.now() );
+
+        //是否默认显示当前时间
+        dateTimePicker.setShowLocalizedDateTime(false);
+
+
+        //设置一个容器
+        final VBox vBox = new VBox();
+        vBox.getChildren().add(dateTimePicker);
+        final Scene scene = new Scene(vBox);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+
+
+        //循环获取选中时间-打印
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        if (dateTimePicker.dateTimeProperty() == null) {
+                            System.out.println("没有选中日期！");
+                        } else {
+                            //获取选中时间
+                            LocalDateTime dateTime = dateTimePicker.dateTimeProperty().get();
+                            System.out.println(dateTime.getYear() + "-" + dateTime.getMonthValue() + "-" + dateTime.getDayOfMonth() + " " + dateTime.getHour() + ":" + dateTime.getMinute() + ":" + dateTime.getSecond());
+                        }
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+//						throw new RuntimeException(e);
+                        continue;
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    static class DateTimePickerTest {
+        public static void main(String[] args) {
+            launch(DateTimePickerMain.class, args);
+        }
+    }
+
+}
