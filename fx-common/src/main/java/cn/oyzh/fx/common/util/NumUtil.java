c6f083967942b53105ec573e1d23467d1bd53639
oyzh <oyzh.1994@qq.com>
2024-07-09 10:36:12 +0800

    1. 细节处理

--- fx-common/src/main/java/cn/oyzh/fx/common/util/NumUtil.java
+++ fx-common/src/main/java/cn/oyzh/fx/common/util/NumUtil.java
@@ -10,52 +10,24 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class NumUtil {
 
-    // private static int compare(String s1, String s2) {
-    //     if (s1.startsWith("-") && !s2.startsWith("-")) {
-    //         return -1;
-    //     }
-    //     if (!s1.startsWith("-") && s2.startsWith("-")) {
-    //         return 1;
-    //     }
-    //     return s1.compareTo(s2);
-    // }
-    //
-    // private static int compare(Number n1, Number n2) {
-    //     if (n1 == null || n2 == null) {
-    //         return -1;
-    //     }
-    //     if (n1 instanceof Float d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = BigDecimal.valueOf(n2.floatValue());
-    //         return b2.toPlainString().compareTo(b1.toPlainString());
-    //     }
-    //     if (n1 instanceof Double d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = BigDecimal.valueOf(n2.doubleValue());
-    //         return compare(b1.toPlainString(), b2.toPlainString());
-    //     }
-    //     if (n1 instanceof Integer d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = new BigDecimal(n2.intValue());
-    //         return b2.toPlainString().compareTo(b1.toPlainString());
-    //     }
-    //     if (n1 instanceof Long d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = new BigDecimal(n2.longValue());
-    //         return b2.toPlainString().compareTo(b1.toPlainString());
-    //     }
-    //     if (n1 instanceof Short d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = new BigDecimal(n2.shortValue());
-    //         return b2.toPlainString().compareTo(b1.toPlainString());
-    //     }
-    //     if (n1 instanceof Byte d1) {
-    //         BigDecimal b1 = new BigDecimal(d1);
-    //         BigDecimal b2 = new BigDecimal(n2.byteValue());
-    //         return b2.toPlainString().compareTo(b1.toPlainString());
-    //     }
-    //     return -1;
-    // }
+    /**
+     * 格式化大小
+     *
+     * @param size 大小
+     * @return 结果
+     */
+    public static String formatSize(long size) {
+        if (size < 1024) {
+            return size + "B";
+        } else if (size < 1024 * 1024) {
+            return size / 1024.d + "KB";
+        } else if (size < 1024 * 1024 * 1024) {
+            return size / 1024.d / 1024 + "MB";
+        } else if (size < 1024 * 1024 * 1024 * 1024L) {
+            return size / 1024.d / 1024 / 1024 + "GB";
+        }
+        return size / 1024.d / 1024 / 1024 / 1024 + "TB";
+    }
 
     public static boolean isLT(Number n1, Number n2) {
         if (n1 == null || n2 == null) {
