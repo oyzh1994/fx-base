92d360e42f933dc7824ea0f6c71c3e1c73ac05b7
oyzh <oyzh.1994@qq.com>
2024-11-15 17:44:57 +0800

    1. 部分类移动 2. 删除部分无效类

--- fx-plus/src/main/java/cn/oyzh/fx/plus/gui/tray/QuitTrayItem.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/gui/tray/QuitTrayItem.java
@@ -1,7 +1,8 @@
-package cn.oyzh.fx.plus.tray;
+package cn.oyzh.fx.plus.gui.tray;
 
 import cn.oyzh.fx.plus.gui.svg.glyph.QuitSVGGlyph;
 import cn.oyzh.fx.plus.i18n.I18nHelper;
+import cn.oyzh.fx.plus.tray.TrayItem;
 
 /**
  * 托盘菜单项
