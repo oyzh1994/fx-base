fc5740d3c6154d9a249007f07b079c5e3e30f7b1
oyzh <oyzh.1994@qq.com>
2024-12-12 16:38:53 +0800

    1. 研究性代码提交

--- fx-plus/src/main/java/cn/oyzh/fx/plus/tray/TrayImage.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/tray/TrayImage.java
@@ -10,7 +10,6 @@ import javafx.scene.Scene;
 import javafx.scene.robot.Robot;
 import javafx.stage.StageStyle;
 import lombok.Setter;
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
@@ -33,9 +32,206 @@ public class TrayImage extends TrayIcon implements ThemeAdapter {
     @Setter
     private TrayMenu menu;
 
+    // private static Class<?> clazz;
+    //
+    // static {
+    //     try {
+    //         clazz = java.lang.Class.forName("sun.awt.windows.WTrayIconPeer");
+    //         // Field field1 = clazz.getDeclaredField("TRAY_ICON_WIDTH");
+    //         // field1.setAccessible(true);
+    //         // field1.set(null, 64);
+    //         // Field field2 = clazz.getDeclaredField("TRAY_ICON_HEIGHT");
+    //         // field2.setAccessible(true);
+    //         // field2.set(null, 64);
+    //         // int maskSize = 64 * 64 / 8;
+    //         // Field field3 = clazz.getDeclaredField("TRAY_ICON_MASK_SIZE");
+    //         // field3.setAccessible(true);
+    //         // field3.set(null, maskSize);
+    //     } catch (ClassNotFoundException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
     public TrayImage(Image image) {
         super(image);
+        // ThreadUtil.start(() -> {
+        //     this.setImage1(image);
+        //
+        // }, 3000);
+        // ThreadUtil.start(() -> {
+        //     this.setImage1(image);
+        //
+        // }, 6000);
     }
+
+    // Object peer = null;
+    //
+    // int size = 32;
+    // // @Override
+    // // public void setImage(Image image) {
+    // //     super.setImage(image);
+    // //     if (peer == null) {
+    // //         Field field = null;
+    // //         try {
+    // //             field = TrayIcon.class.getDeclaredField("peer");
+    // //             field.setAccessible(true);
+    // //             peer = field.get(this);
+    // //         } catch (NoSuchFieldException e) {
+    // //             throw new RuntimeException(e);
+    // //         } catch (IllegalAccessException e) {
+    // //             throw new RuntimeException(e);
+    // //         }
+    // //     }
+    // //     if (peer == null) {
+    // //         return;
+    // //     }
+    // //
+    // //     BufferedImage bufImage = new BufferedImage(size,
+    // //             size, BufferedImage.TYPE_INT_ARGB);
+    // //     Graphics2D gr = bufImage.createGraphics();
+    // //     if (gr != null) {
+    // //         try {
+    // //             gr.setPaintMode();
+    // //
+    // //             gr.drawImage(image, 0, 0, size,
+    // //                     size, null);
+    // //             createNativeImage(bufImage);
+    // //
+    // //
+    // //             // Method method = clazz.getDeclaredMethod("updateNativeIcon");
+    // //             // method.setAccessible(true);
+    // //             // method.invoke(peer, false);
+    // //
+    // //             Method[] methods = clazz.getDeclaredMethods();
+    // //             for (Method method : methods) {
+    // //                 if (peer != null && method.getName().equals("updateNativeIcon")) {
+    // //                     method.setAccessible(true);
+    // //                     // method.invoke(peer, false);
+    // //                     method.invoke(peer, true);
+    // //                     System.out.println("-------------------");
+    // //                     break;
+    // //                 }
+    // //             }
+    // //         } catch (InvocationTargetException e) {
+    // //             throw new RuntimeException(e);
+    // //         } catch (IllegalAccessException e) {
+    // //             throw new RuntimeException(e);
+    // //         } catch (NoSuchMethodException e) {
+    // //             throw new RuntimeException(e);
+    // //         } catch (NoSuchFieldException e) {
+    // //             throw new RuntimeException(e);
+    // //         } finally {
+    // //             gr.dispose();
+    // //         }
+    // //     }
+    // // }
+    //
+    // @Override
+    // public void setImage(Image image) {
+    //     super.setImage(image);
+    // }
+    //
+    // void setImage1(Image image){
+    //     if (peer == null) {
+    //         Field field = null;
+    //         try {
+    //             field = TrayIcon.class.getDeclaredField("peer");
+    //             field.setAccessible(true);
+    //             peer = field.get(this);
+    //         } catch (NoSuchFieldException e) {
+    //             throw new RuntimeException(e);
+    //         } catch (IllegalAccessException e) {
+    //             throw new RuntimeException(e);
+    //         }
+    //     }
+    //     if (peer == null) {
+    //         return;
+    //     }
+    //
+    //     boolean autosize = this.isImageAutoSize();
+    //     AffineTransform tx = GraphicsEnvironment.getLocalGraphicsEnvironment().
+    //             getDefaultScreenDevice().getDefaultConfiguration().
+    //             getDefaultTransform();
+    //     int w = Region.clipScale(size, tx.getScaleX());
+    //     int h = Region.clipScale(size, tx.getScaleY());
+    //     int imgWidth = Region.clipScale(image.getWidth(null), tx.getScaleX());
+    //     int imgHeight = Region.clipScale(image.getHeight(null), tx.getScaleY());
+    //     BufferedImage bufImage = new BufferedImage(w,
+    //             h, BufferedImage.TYPE_INT_ARGB);
+    //     Graphics2D gr = bufImage.createGraphics();
+    //     if (gr != null) {
+    //         try {
+    //             gr.setPaintMode();
+    //
+    //             gr.drawImage(image, 0, 0, (autosize ? w : imgWidth),
+    //                     (autosize ? h : imgHeight), null);
+    //             createNativeImage(bufImage);
+    //
+    //
+    //             // Method method = clazz.getDeclaredMethod("updateNativeIcon");
+    //             // method.setAccessible(true);
+    //             // method.invoke(peer, false);
+    //
+    //             Method[] methods = clazz.getDeclaredMethods();
+    //             for (Method method : methods) {
+    //                 if (peer != null && method.getName().equals("updateNativeIcon")) {
+    //                     method.setAccessible(true);
+    //                     // method.invoke(peer, false);
+    //                     method.invoke(peer, false);
+    //                     System.out.println("-------------------");
+    //                     break;
+    //                 }
+    //             }
+    //         } catch (InvocationTargetException e) {
+    //             throw new RuntimeException(e);
+    //         } catch (IllegalAccessException e) {
+    //             throw new RuntimeException(e);
+    //         } catch (NoSuchMethodException e) {
+    //             throw new RuntimeException(e);
+    //         } catch (NoSuchFieldException e) {
+    //             throw new RuntimeException(e);
+    //         } finally {
+    //             gr.dispose();
+    //         }
+    //     }
+    // }
+    // void createNativeImage(BufferedImage bimage) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+    //     Raster raster = bimage.getRaster();
+    //     byte[] andMask = new byte[size * size / 8];
+    //     // byte[] andMask = new byte[64 * 64 / 8];
+    //     int[] pixels = ((DataBufferInt) raster.getDataBuffer()).getData();
+    //     int npixels = pixels.length;
+    //     int ficW = raster.getWidth();
+    //
+    //     for (int i = 0; i < npixels; i++) {
+    //         int ibyte = i / 8;
+    //         int omask = 1 << (7 - (i % 8));
+    //
+    //         if ((pixels[i] & 0xff000000) == 0) {
+    //             // Transparent bit
+    //             if (ibyte < andMask.length) {
+    //                 andMask[ibyte] |= omask;
+    //             }
+    //         }
+    //     }
+    //
+    //     if (raster instanceof IntegerComponentRaster) {
+    //         ficW = ((IntegerComponentRaster) raster).getScanlineStride();
+    //     }
+    //
+    //
+    //     Method[] methods = clazz.getDeclaredMethods();
+    //     for (Method method : methods) {
+    //         if (peer != null && method.getName().equals("setNativeIcon")) {
+    //             // Method method = clazz.getMethod("setNativeIcon");
+    //             method.setAccessible(true);
+    //             method.invoke(peer, ((DataBufferInt) bimage.getRaster().getDataBuffer()).getData(),
+    //                     andMask, ficW, raster.getWidth(), raster.getHeight());
+    //             System.out.println("-------------------1");
+    //             break;
+    //         }
+    //     }
+    // }
 
     /**
      * 获取菜单
