4f5b2d60fa5dcc55dc3e8114fe93f0768473b341
oyzh <oyzh.1994@qq.com>
2024-12-23 15:12:12 +0800

    1. 组件完善

--- fx-plus/src/main/java/cn/oyzh/fx/plus/FXConst.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/FXConst.java
@@ -1,5 +1,8 @@
 package cn.oyzh.fx.plus;
 
+import javafx.application.Application;
+import javafx.application.HostServices;
+
 /**
  * fx常量
  *
@@ -8,30 +11,35 @@ package cn.oyzh.fx.plus;
  */
 public class FXConst {
 
-    /**
-     * tab地址
-     */
-    public final static String TAB_PATH = "/tabs/";
+    // /**
+    //  * tab地址
+    //  */
+    // public final static String TAB_PATH = "/tabs/";
 
     /**
      * 页面地址
      */
     public final static String VIEW_PATH = "/views/";
 
-    /**
-     * 页面地址
-     */
-    public final static String MODULE_PATH = "/module/";
-
-    /**
-     * popup地址
-     */
-    public final static String POPUP_PATH = "/popups/";
+    // /**
+    //  * 页面地址
+    //  */
+    // public final static String MODULE_PATH = "/module/";
+    //
+    // /**
+    //  * popup地址
+    //  */
+    // public final static String POPUP_PATH = "/popups/";
 
     /**
      * 提示标题属性
      */
     public final static String TOOLTIP_PROP_KEY = "javafx.scene.control.Tooltip";
+
+    /**
+     * 应用实例
+     */
+    public static Application INSTANCE;
 
     /**
      * app图标
@@ -56,4 +64,12 @@ public class FXConst {
         System.setProperty(APP_ICON, appIcon);
     }
 
+    /**
+     * 获取host服务
+     *
+     * @return HostServices
+     */
+    public static HostServices getHostServices() {
+        return INSTANCE == null ? null : INSTANCE.getHostServices();
+    }
 }
---
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/controls/button/FXHyperlink.java
@@ -0,0 +1,54 @@
+package cn.oyzh.fx.plus.controls.button;
+
+import cn.oyzh.common.log.JulLog;
+import cn.oyzh.common.util.StringUtil;
+import cn.oyzh.fx.plus.FXConst;
+import cn.oyzh.fx.plus.adapter.StateAdapter;
+import cn.oyzh.fx.plus.adapter.TipAdapter;
+import cn.oyzh.fx.plus.font.FontAdapter;
+import cn.oyzh.fx.plus.mouse.MouseAdapter;
+import cn.oyzh.fx.plus.node.NodeAdapter;
+import cn.oyzh.fx.plus.node.NodeGroup;
+import cn.oyzh.fx.plus.node.NodeManager;
+import cn.oyzh.fx.plus.theme.ThemeAdapter;
+import javafx.application.HostServices;
+import javafx.scene.Cursor;
+import javafx.scene.control.Hyperlink;
+
+/**
+ * @author oyzh
+ * @since 2024-12-23
+ */
+public class FXHyperlink extends Hyperlink implements MouseAdapter, NodeGroup, NodeAdapter, ThemeAdapter, TipAdapter, StateAdapter, FontAdapter {
+
+    {
+        NodeManager.init(this);
+    }
+
+    public FXHyperlink() {
+        super();
+    }
+
+    public FXHyperlink(String text) {
+        super(text);
+    }
+
+    @Override
+    public void initNode() {
+        this.setCursor(Cursor.HAND);
+        this.setPickOnBounds(true);
+        this.setMnemonicParsing(false);
+        this.setFocusTraversable(false);
+        this.setOnMousePrimaryClicked(event -> {
+            String url = this.getText();
+            if (StringUtil.isNotBlank(url)) {
+                HostServices services = FXConst.getHostServices();
+                if (services != null) {
+                    services.showDocument(url);
+                } else {
+                    JulLog.warn("HostServices is null");
+                }
+            }
+        });
+    }
+}
