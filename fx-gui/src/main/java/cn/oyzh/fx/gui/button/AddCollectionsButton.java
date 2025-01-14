26dad76efaa1fc54e7c900184bcdb0ad704bdea0
oyzh <oyzh.1994@q.qcom>
2025-01-14 17:22:24 +0800

    1. 组件完善

---
+++ fx-gui/src/main/java/cn/oyzh/fx/gui/button/AddCollectionsButton.java
@@ -0,0 +1,23 @@
+package cn.oyzh.fx.gui.button;
+
+
+import cn.oyzh.fx.gui.svg.glyph.AddSVGGlyph;
+import cn.oyzh.fx.plus.controls.button.IconButton;
+import cn.oyzh.i18n.I18nHelper;
+
+/**
+ * @author oyzh
+ * @since 2024/04/08
+ */
+public class AddCollectionsButton extends IconButton {
+
+    @Override
+    public void initNode() {
+        this.setPrefHeight(25);
+        this.addClass("success");
+        this.setText(I18nHelper.addCollections());
+        this.setTipText(I18nHelper.addCollections());
+        this.init(new AddSVGGlyph(), 0.7);
+        super.initNode();
+    }
+}
