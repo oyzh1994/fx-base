cc6c0d5b8ec01f97028ce9d9f0b5951f6442c6b9
oyzh <oyzh.1994@q.qcom>
2025-02-10 14:49:16 +0800

    1. 细节调整

--- fx-plus/src/main/java/cn/oyzh/fx/plus/controls/tree/view/FXTreeItem.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/controls/tree/view/FXTreeItem.java
@@ -89,7 +89,8 @@ public abstract class FXTreeItem<V extends FXTreeItemValue> extends TreeItem<V> 
     /**
      * 开始等待
      *
-     * @param task 待执行业务
+     * @param task      待执行业务
+     * @param autoClose 自动关闭动画
      */
     public void startWaiting(Runnable task, boolean autoClose) {
         if (this.itemGraphic() instanceof SVGGlyph glyph) {
@@ -111,9 +112,10 @@ public abstract class FXTreeItem<V extends FXTreeItemValue> extends TreeItem<V> 
     /**
      * 开始等待
      *
-     * @param task 待执行业务
+     * @param task  待执行业务
+     * @param delay 延迟时间
      */
-    public void startWaiting(Runnable task, int timeout) {
+    public void startWaiting(Runnable task, int delay) {
         if (this.itemGraphic() instanceof SVGGlyph glyph) {
             glyph.startWaiting();
             TaskManager.startDelay(() -> {
@@ -122,9 +124,9 @@ public abstract class FXTreeItem<V extends FXTreeItemValue> extends TreeItem<V> 
                         task.run();
                     }
                 } finally {
-                    TaskManager.startDelay(glyph::stopWaiting, timeout);
+                    TaskManager.start(glyph::stopWaiting);
                 }
-            }, 50);
+            }, delay);
         }
     }
 
