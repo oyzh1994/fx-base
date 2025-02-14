dc62f4ef96d430a90b354aeca6345e80f5ba059f
oyzh <oyzh.1994@q.qcom>
2025-02-14 10:01:15 +0800

    1. 组件完善

---
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/controls/FXGroup.java
@@ -0,0 +1,12 @@
+package cn.oyzh.fx.plus.controls;
+
+import cn.oyzh.fx.plus.adapter.StateAdapter;
+import cn.oyzh.fx.plus.node.NodeAdapter;
+import javafx.scene.Group;
+
+/**
+ * @author oyzh
+ * @since 2025-02-14
+ */
+public class FXGroup extends Group implements NodeAdapter, StateAdapter {
+}
