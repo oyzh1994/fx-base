42fd5be3c72db10282385353443c20899aecf110
oyzh <oyzh.1994@q.qcom>
2025-02-20 11:42:24 +0800

    1. 组件完善

---
+++ fx-rich/src/main/java/cn/oyzh/fx/rich/richtextfx/json/RichJsonTextArea.java
@@ -0,0 +1,10 @@
+package cn.oyzh.fx.rich.richtextfx.json;
+
+import cn.oyzh.fx.rich.richtextfx.control.BaseRichTextArea;
+
+/**
+ * @author oyzh
+ * @since 2025-02-20
+ */
+public class RichJsonTextArea extends BaseRichTextArea {
+}
--- fx-rich/src/main/java/cn/oyzh/fx/rich/richtextfx/json/RichJsonTextAreaPane.java
+++ fx-rich/src/main/java/cn/oyzh/fx/rich/richtextfx/json/RichJsonTextAreaPane.java
@@ -1,78 +1,77 @@
-//package cn.oyzh.fx.rich.richtextfx.json;
-//
-//import cn.oyzh.common.json.JSONUtil;
-//import cn.oyzh.common.util.RegexHelper;
-//import cn.oyzh.common.util.StringUtil;
-//import cn.oyzh.fx.plus.util.FXUtil;
-//import cn.oyzh.fx.rich.RichTextStyle;
-//import cn.oyzh.fx.rich.richtextfx.control.BaseRichTextArea;
-//import cn.oyzh.fx.rich.richtextfx.control.RichTextAreaPane;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.regex.Matcher;
-//
-///**
-// * @author oyzh
-// * @since 2024/5/17
-// */
-//public class RichJsonTextAreaPane extends RichTextAreaPane<BaseRichTextArea> {
-//
-//    public RichJsonTextAreaPane() {
-//        super(new BaseRichTextArea());
-//    }
-//
-//    @Override
-//    protected void initTextArea() {
-//        super.initTextArea();
-//        this.showLineNum();
-//        this.addTextChangeListener((observable, oldValue, newValue) -> this.initTextStyle());
-//    }
-//
-//    @Override
-//    public void initTextStyle() {
-//        FXUtil.runWait(() -> {
-//            this.clearTextStyle();
-//            String text = this.getText();
-//            Matcher matcher1 = RegexHelper.jsonSymbolPattern().matcher(text);
-//            List<RichTextStyle> styles = new ArrayList<>();
-//            while (matcher1.find()) {
-//                styles.add(new RichTextStyle(matcher1.start(), matcher1.end(), "-fx-fill: #4169E1;"));
+package cn.oyzh.fx.rich.richtextfx.json;
+
+import cn.oyzh.common.json.JSONUtil;
+import cn.oyzh.common.util.RegexHelper;
+import cn.oyzh.common.util.StringUtil;
+import cn.oyzh.fx.plus.util.FXUtil;
+import cn.oyzh.fx.rich.RichTextStyle;
+import cn.oyzh.fx.rich.richtextfx.control.RichTextAreaPane;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+/**
+ * @author oyzh
+ * @since 2024/5/17
+ */
+public class RichJsonTextAreaPane extends RichTextAreaPane<RichJsonTextArea> {
+
+    public RichJsonTextAreaPane() {
+        super(new RichJsonTextArea());
+    }
+
+    @Override
+    protected void initTextArea() {
+        super.initTextArea();
+        this.showLineNum();
+        this.addTextChangeListener((observable, oldValue, newValue) -> this.initTextStyle());
+    }
+
+    @Override
+    public void initTextStyle() {
+        FXUtil.runWait(() -> {
+            this.clearTextStyle();
+            String text = this.getText();
+            Matcher matcher1 = RegexHelper.jsonSymbolPattern().matcher(text);
+            List<RichTextStyle> styles = new ArrayList<>();
+            while (matcher1.find()) {
+                styles.add(new RichTextStyle(matcher1.start(), matcher1.end(), "-fx-fill: #4169E1;"));
+            }
+            Matcher matcher2 = RegexHelper.jsonKeyPattern().matcher(text);
+            while (matcher2.find()) {
+                styles.add(new RichTextStyle(matcher2.start(), matcher2.end(), "-fx-fill: #EE2C2C;"));
+            }
+            Matcher matcher3 = RegexHelper.jsonValuePattern().matcher(text);
+            while (matcher3.find()) {
+                styles.add(new RichTextStyle(matcher3.start(), matcher3.end(), "-fx-fill: green;"));
+            }
+//            for (RichTextStyle style : styles) {
+            this.setStyles(styles);
 //            }
-//            Matcher matcher2 = RegexHelper.jsonKeyPattern().matcher(text);
-//            while (matcher2.find()) {
-//                styles.add(new RichTextStyle(matcher2.start(), matcher2.end(), "-fx-fill: #EE2C2C;"));
-//            }
-//            Matcher matcher3 = RegexHelper.jsonValuePattern().matcher(text);
-//            while (matcher3.find()) {
-//                styles.add(new RichTextStyle(matcher3.start(), matcher3.end(), "-fx-fill: green;"));
-//            }
-////            for (RichTextStyle style : styles) {
-//                this.setStyles(styles);
-////            }
-//        });
-//    }
-//
-//    public void setJsonStr(String jsonStr) {
-//        if (StringUtil.isEmpty(jsonStr)) {
-//            return;
-//        }
-//        if (!JSONUtil.isJson(jsonStr)) {
-//            this.requestFocus();
-//            throw new RuntimeException("invalid json text!");
-//        }
-//        this.setText(JSONUtil.toPretty(jsonStr));
-//    }
-//
-//    public String getJsonStr() {
-//        String text = this.getText();
-//        if (StringUtil.isEmpty(text)) {
-//            return null;
-//        }
-//        if (!JSONUtil.isJson(text)) {
-//            this.requestFocus();
-//            throw new RuntimeException("invalid json text!");
-//        }
-//        return JSONUtil.toJson(text);
-//    }
-//}
+        });
+    }
+
+    public void setJsonStr(String jsonStr) {
+        if (StringUtil.isEmpty(jsonStr)) {
+            return;
+        }
+        if (!JSONUtil.isJson(jsonStr)) {
+            this.requestFocus();
+            throw new RuntimeException("invalid json text!");
+        }
+        this.setText(JSONUtil.toPretty(jsonStr));
+    }
+
+    public String getJsonStr() {
+        String text = this.getText();
+        if (StringUtil.isEmpty(text)) {
+            return null;
+        }
+        if (!JSONUtil.isJson(text)) {
+            this.requestFocus();
+            throw new RuntimeException("invalid json text!");
+        }
+        return JSONUtil.toJson(text);
+    }
+}
