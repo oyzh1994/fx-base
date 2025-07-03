1ebc031c8b8eb3f48fee4a0a13b2e995093ff1dd
oyzh <oyzh.1994@qq.com>
2025-07-03 22:13:42 +0800

    1. 组件完善

--- fx-gui/src/main/java/cn/oyzh/fx/gui/menu/MenuItemHelper.java
+++ fx-gui/src/main/java/cn/oyzh/fx/gui/menu/MenuItemHelper.java
@@ -214,6 +214,10 @@ public class MenuItemHelper {
         return (FXMenuItem) MenuItemManager.getMenuItem(I18nHelper.saveImage(), new ExportSVGGlyph(iconSize), action);
     }
 
+    public static FXMenuItem saveContainer(String iconSize, Runnable action) {
+        return (FXMenuItem) MenuItemManager.getMenuItem(I18nHelper.saveContainer(), new ExportSVGGlyph(iconSize), action);
+    }
+
     public static FXMenuItem exportConnect(String iconSize, Runnable action) {
         return (FXMenuItem) MenuItemManager.getMenuItem(I18nHelper.exportConnect(), new ExportSVGGlyph(iconSize), action);
     }
