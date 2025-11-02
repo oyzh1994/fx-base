9a2103a38268a0140ff3209620e78723e1329953
oyzh <oyzh.1994@qq.com>
2025-11-02 15:08:09 +0800

    1. 打包支持toml

--- fx-pkg/src/main/java/cn/oyzh/fx/pkg/PackCost.java
+++ fx-pkg/src/main/java/cn/oyzh/fx/pkg/PackCost.java
@@ -7,10 +7,24 @@ package cn.oyzh.fx.pkg;
  */
 public class PackCost {
 
+    /**
+     * 打包最终目录
+     */
     public static final String DEST = "dest";
 
+    /**
+     * github最终目录
+     */
     public static final String GITHUB_DIST = "githubDist";
 
+    /**
+     * 打包工程，即fx-pkg工程的路径
+     */
+    public static final String PKG_PATH = "pkgPath";
+
+    /**
+     * 项目工程，即需要打包工程的路径
+     */
     public static final String PROJECT_PATH = "projectPath";
 
 }
