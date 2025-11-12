ba0a5bc6da9334573579bde9d2b81fbbc9ed8a77
oyzh <oyzh.1994@q.qcom>
2025-11-12 14:55:16 +0800

    1. optimize: 事件总线相关改造

--- fx-plus/src/main/java/cn/oyzh/fx/plus/event/FXEventBus.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/event/FXEventBus.java
@@ -1,6 +1,5 @@
 package cn.oyzh.fx.plus.event;
 
-import cn.oyzh.common.thread.TaskManager;
 import cn.oyzh.event.EventBus;
 import cn.oyzh.event.EventConfig;
 import cn.oyzh.fx.plus.util.FXUtil;
@@ -9,11 +8,11 @@ import cn.oyzh.fx.plus.util.FXUtil;
  * @author oyzh
  * @since 2024-11-14
  */
-public class FxEventBus extends EventBus {
+public class FXEventBus extends EventBus {
 
     @Override
     public <C extends EventConfig> void post(Object event, C config, Integer delayMillis) {
-        if (config instanceof FxEventConfig fxConfig) {
+        if (config instanceof FXEventConfig fxConfig&&fxConfig.isFxThread()) {
             boolean verbose = config.isVerbose();
             boolean delay = delayMillis != null && delayMillis > 0;
             boolean async = config.isAsync();
@@ -21,21 +20,24 @@ public class FxEventBus extends EventBus {
             Runnable func = () -> this.doEventPost(event, verbose);
             // 延迟、异步、fx线程
             if (delay && async && fxThread) {
-                TaskManager.startDelay(() -> FXUtil.runWait(func), delayMillis);
-            } else if (delay && async) {// 延迟、异步
-                TaskManager.startDelay(func, delayMillis);
+                FXUtil.runLater(func, delayMillis);
+                // TaskManager.startDelay(() -> FXUtil.runLater(func,delayMillis), delayMillis);
+            // } else if (delay && async) {// 延迟、异步
+            //     TaskManager.startDelay(func, delayMillis);
             } else if (delay && fxThread) {// 延迟、fx线程
-                TaskManager.startDelay(() -> FXUtil.runWait(func), delayMillis);
+                FXUtil.runLater(func, delayMillis);
+                // TaskManager.startDelay(() -> FXUtil.runWait(func), delayMillis);
             } else if (async && fxThread) {// 异步、fx线程
-                TaskManager.startSync(() -> FXUtil.runWait(func));
-            } else if (delay) {// 延迟
-                TaskManager.startDelay(func, delayMillis);
-            } else if (async) {// 异步
-                TaskManager.startSync(func);
+                FXUtil.runLater(func);
+                // TaskManager.startSync(() -> FXUtil.runWait(func));
+            // } else if (delay) {// 延迟
+            //     TaskManager.startDelay(func, delayMillis);
+            // } else if (async) {// 异步
+            //     TaskManager.startSync(func);
             } else if (fxThread) {// fx线程
                 FXUtil.runWait(func);
-            } else {// 正常执行
-                func.run();
+            // } else {// 正常执行
+            //     func.run();
             }
         } else {
             super.post(event, config, delayMillis);
--- fx-plus/src/main/java/cn/oyzh/fx/plus/event/FXEventConfig.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/event/FXEventConfig.java
@@ -6,23 +6,35 @@ import cn.oyzh.event.EventConfig;
  * @author oyzh
  * @since 2024/3/29
  */
-public class FxEventConfig extends EventConfig {
+public class FXEventConfig extends EventConfig {
 
+    /**
+     * 是否fx线程
+     */
     private Boolean fxThread;
 
     public void setFxThread(Boolean fxThread) {
         this.fxThread = fxThread;
     }
 
-    public static FxEventConfig SYNC = new FxEventConfig();
-
-    public static FxEventConfig ASYNC = new FxEventConfig();
-
-    public static FxEventConfig DEFAULT = new FxEventConfig();
-
     public boolean isFxThread() {
         return fxThread != null && fxThread;
     }
+
+    /**
+     * 同步配置
+     */
+    public static FXEventConfig SYNC = new FXEventConfig();
+
+    /**
+     * 异步配置
+     */
+    public static FXEventConfig ASYNC = new FXEventConfig();
+
+    /**
+     * 默认配置
+     */
+    public static FXEventConfig DEFAULT = new FXEventConfig();
 
     static {
         SYNC.async = false;
@@ -36,6 +48,5 @@ public class FxEventConfig extends EventConfig {
         DEFAULT.async = true;
         DEFAULT.verbose = true;
         DEFAULT.fxThread = true;
-
     }
 }
