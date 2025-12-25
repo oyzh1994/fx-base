255c905ade96a0c4f119d25f2a0a9bb20b143f36
oyzh <oyzh.1994@q.qcom>
2025-12-25 16:54:29 +0800

    1. optimize: 组件完善

---
+++ fx-gui/src/main/java/cn/oyzh/fx/gui/svg/glyph/UploadDownloadSVGGlyph.java
@@ -0,0 +1,19 @@
+package cn.oyzh.fx.gui.svg.glyph;
+
+import cn.oyzh.fx.plus.controls.svg.SVGGlyph;
+
+/**
+ * @author oyzh
+ * @since 2024/4/11
+ */
+public class UploadDownloadSVGGlyph extends SVGGlyph {
+
+    public UploadDownloadSVGGlyph() {
+        this.setUrl("/fx-svg/upload-download.svg");
+    }
+
+    public UploadDownloadSVGGlyph(String size) {
+        this();
+        this.setSizeStr(size);
+    }
+}
