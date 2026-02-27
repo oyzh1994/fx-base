2002f2b9f5657956d7bfb29ce1b8d5efad00c6de
oyzh <oyzh.1994@q.qcom>
2026-02-27 16:21:14 +0800

    update: 注释不用类库

--- fx-pkg/src/main/java/cn/oyzh/fx/pkg/config/PackConfigParser.java
+++ fx-pkg/src/main/java/cn/oyzh/fx/pkg/config/PackConfigParser.java
@@ -13,9 +13,7 @@ import cn.oyzh.fx.pkg.jpackage.JPackageConfig;
 import cn.oyzh.fx.pkg.jpackage.JPackageConfigParser;
 import cn.oyzh.fx.pkg.jre.JreConfig;
 import cn.oyzh.fx.pkg.jre.JreConfigParser;
-import cn.oyzh.fx.pkg.packr.PackrConfigParser;
 import com.alibaba.fastjson2.JSONObject;
-import com.badlogicgames.packr.PackrConfig;
 
 /**
  * 打包配置解析器
--- fx-terminal/src/main/java/cn/oyzh/fx/terminal/TerminalTextArea.java
+++ fx-terminal/src/main/java/cn/oyzh/fx/terminal/TerminalTextArea.java
@@ -1,20 +1,20 @@
-package cn.oyzh.fx.terminal;
-
-import cn.oyzh.fx.rich.richtextfx.control.BaseRichTextArea;
-
-/**
- * 富文本数据文本域
- *
- * @author oyzh
- * @since 2024/04/17
- */
-@Deprecated
-public class TerminalTextArea extends BaseRichTextArea {
-
-    @Override
-    public void initNode() {
-        super.initNode();
-        this.disableFont();
-        // this.initTextStyle();
-    }
-}
+//package cn.oyzh.fx.terminal;
+//
+//import cn.oyzh.fx.rich.richtextfx.control.BaseRichTextArea;
+//
+///**
+// * 富文本数据文本域
+// *
+// * @author oyzh
+// * @since 2024/04/17
+// */
+//@Deprecated
+//public class TerminalTextArea extends BaseRichTextArea {
+//
+//    @Override
+//    public void initNode() {
+//        super.initNode();
+//        this.disableFont();
+//        // this.initTextStyle();
+//    }
+//}
