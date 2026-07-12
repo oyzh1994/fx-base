644df52018095fc28ad84fc1adcb54135d413e31
oyzh <oyzh.1994@qq.com>
2026-07-12 15:29:34 +0800

    add: Hex组件测试

---
+++ fx-plus/src/test/java/cn/oyzh/fx/plus/test/hex/HexViewerAppTest.java
@@ -0,0 +1,11 @@
+package cn.oyzh.fx.plus.test.hex;
+
+import java.net.URISyntaxException;
+
+public class HexViewerAppTest {
+
+    public static void main(String[] args) throws URISyntaxException {
+        HexViewer.main(args);
+    }
+
+}
