89f7d3c97f2be25935868e17f4dda83d3c9c80ff
oyzh <oyzh.1994@q.qcom>
2026-07-14 10:20:20 +0800

    update: 细节调整

--- fx-plus/src/main/java/cn/oyzh/fx/gui/svg/pane/LayoutSVGPane2.java
+++ fx-plus/src/main/java/cn/oyzh/fx/gui/svg/pane/LayoutSVGPane2.java
@@ -1,45 +1,45 @@
-package cn.oyzh.fx.gui.svg.pane;
-
-import cn.oyzh.fx.gui.svg.glyph.layout.Layout1SVGGlyph;
-import cn.oyzh.fx.gui.svg.glyph.layout.Layout2SVGGlyph;
-import cn.oyzh.fx.plus.controls.svg.SVGGlyph;
-import cn.oyzh.fx.plus.controls.svg.SVGLabel;
-import cn.oyzh.fx.plus.controls.svg.SVGPane;
-import javafx.scene.text.Font;
-
-/**
- * @author oyzh
- * @since 2024-12-09
- */
-public class LayoutSVGPane2 extends SVGPane {
-
-    public LayoutSVGPane2() {
-        this.layout1();
-    }
-
-    public void layout1() {
-        this.setChild(new Layout1SVGGlyph(this.getSize()));
-    }
-
-    public void layout2() {
-        this.setChild(new Layout2SVGGlyph(this.getSize()));
-    }
-
-    public boolean isLayout1() {
-        SVGLabel label = (SVGLabel) this.getChildren().getFirst();
-        SVGGlyph svgGlyph = label.graphic();
-        return svgGlyph.getUrl().contains("layout1.svg");
-    }
-
-    @Override
-    public void changeFont(Font font) {
-        if (!this.isChildEmpty() && this.isEnableFont()) {
-            if (this.isLayout1()) {
-                this.layout1();
-            } else {
-                this.layout2();
-            }
-        }
-        super.changeFont(font);
-    }
-}
+//package cn.oyzh.fx.gui.svg.pane;
+//
+//import cn.oyzh.fx.gui.svg.glyph.layout.Layout1SVGGlyph;
+//import cn.oyzh.fx.gui.svg.glyph.layout.Layout2SVGGlyph;
+//import cn.oyzh.fx.plus.controls.svg.SVGGlyph;
+//import cn.oyzh.fx.plus.controls.svg.SVGLabel;
+//import cn.oyzh.fx.plus.controls.svg.SVGPane;
+//import javafx.scene.text.Font;
+//
+///**
+// * @author oyzh
+// * @since 2024-12-09
+// */
+//public class LayoutSVGPane2 extends SVGPane {
+//
+//    public LayoutSVGPane2() {
+//        this.layout1();
+//    }
+//
+//    public void layout1() {
+//        this.setChild(new Layout1SVGGlyph(this.getSize()));
+//    }
+//
+//    public void layout2() {
+//        this.setChild(new Layout2SVGGlyph(this.getSize()));
+//    }
+//
+//    public boolean isLayout1() {
+//        SVGLabel label = (SVGLabel) this.getChildren().getFirst();
+//        SVGGlyph svgGlyph = label.graphic();
+//        return svgGlyph.getUrl().contains("layout1.svg");
+//    }
+//
+//    @Override
+//    public void changeFont(Font font) {
+//        if (!this.isChildEmpty() && this.isEnableFont()) {
+//            if (this.isLayout1()) {
+//                this.layout1();
+//            } else {
+//                this.layout2();
+//            }
+//        }
+//        super.changeFont(font);
+//    }
+//}
