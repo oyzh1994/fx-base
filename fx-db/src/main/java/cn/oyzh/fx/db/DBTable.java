2a8b506c2bf77477bfb6cc9e6765877ea04ca00f
oyzh <oyzh.1994@q.qcom>
2026-09-02 11:13:00 +0800

    update: 适配fx-db模块

---
+++ fx-db/src/main/java/cn/oyzh/fx/db/DBTable.java
@@ -0,0 +1,46 @@
+package cn.oyzh.fx.db;
+
+/**
+ *
+ * @author oyzh
+ * @since 2026-08-21
+ */
+public interface DBTable {
+
+    /**
+     * 获取名称
+     *
+     * @return 结果
+     */
+    String getName();
+
+    /**
+     * 设置名称
+     *
+     * @param name 名称
+     */
+    void setName(String name);
+
+    /**
+     * 设置注释
+     *
+     * @param comment 注释
+     */
+    void setComment(String comment);
+
+    /**
+     * 获取注释
+     *
+     * @return 结果
+     */
+    String getComment();
+
+    /**
+     * 是否有注释
+     *
+     * @return 结果
+     */
+    default boolean hasComment() {
+        return this.getComment() != null;
+    }
+}
