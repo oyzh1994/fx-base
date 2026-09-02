fc74436b2ee88c8c00bfef9be3ba9abd2b03f863
oyzh <oyzh.1994@q.qcom>
2026-09-02 15:56:55 +0800

    update: 适配fx-db模块

---
+++ fx-db/src/main/java/cn/oyzh/fx/db/DBCheck.java
@@ -0,0 +1,34 @@
+package cn.oyzh.fx.db;
+
+
+import cn.oyzh.common.util.StringUtil;
+
+/**
+ * @author oyzh
+ * @since 2024/1/30
+ */
+public interface DBCheck {
+
+    /**
+     * 获取名称
+     *
+     * @return 结果
+     */
+    String getName();
+
+    /**
+     * 设置名称
+     *
+     * @param name 名称
+     */
+    void setName(String name);
+
+    /**
+     * 是否无效
+     *
+     * @return 结果
+     */
+    default boolean isInvalid() {
+        return StringUtil.isBlank(this.getName());
+    }
+}
