09c341445c8dd1945bd6941f139108812f93cf92
oyzh <oyzh.1994@q.qcom>
2026-09-07 14:14:45 +0800

    fix: sql参数化不符合预期

--- fx-db/src/main/java/cn/oyzh/fx/db/util/DBDataUtil.java
+++ fx-db/src/main/java/cn/oyzh/fx/db/util/DBDataUtil.java
@@ -1,7 +1,12 @@
 package cn.oyzh.fx.db.util;
 
+import cn.oyzh.common.date.DateHelper;
+import cn.oyzh.common.util.HexUtil;
+import cn.oyzh.common.util.TextUtil;
 import cn.oyzh.fx.db.DBColumn;
 import cn.oyzh.fx.db.DBDialect;
+
+import java.util.Date;
 
 /**
  *
@@ -166,6 +171,66 @@ public class DBDataUtil {
     //        return "\"" + value + "\"";
     //    }
 
+    //    /**
+    //     * 参数化，sql
+    //     *
+    //     * @param column 字段
+    //     * @param value  值
+    //     * @return 参数化后的值
+    //     */
+    //    public static Object parameterizedForSql(DBColumn column, Object value, DBDialect dialect) {
+    //        //        if (value == null) {
+    //        //            return "NULL";
+    //        //        }
+    //        //        if (column.supportGeometry()) {
+    //        //            return "ST_GeomFromText('" + value + "')";
+    //        //        }
+    //        //        if (column.isDateType()) {
+    //        //            Date date = (Date) value;
+    //        //            return "'" + DateHelper.formatDate(date) + "'";
+    //        //        }
+    //        //        if (column.supportTimestamp()) {
+    //        //            return "'" + value + "'";
+    //        //        }
+    //        //        if (column.supportJson()) {
+    //        //            return "'" + value + "'";
+    //        //        }
+    //        //        if (column.supportBinary()) {
+    //        //            byte[] bytes = (byte[]) value;
+    //        //            if (bytes.length == 0) {
+    //        //                return "NULL";
+    //        //            }
+    //        //            return "0x" + HexUtil.encodeHexStr(bytes, false);
+    //        //        }
+    //        //        if (column.supportBoolean()) {
+    //        //            if (value instanceof Boolean b) {
+    //        //                return b ? "1" : "0";
+    //        //            }
+    //        //        }
+    //        //        if (column.supportBit()) {
+    //        //            if (value instanceof Boolean b) {
+    //        //                return b ? "1" : "0";
+    //        //            }
+    //        //            byte[] bytes = (byte[]) value;
+    //        //            if (bytes.length == 0) {
+    //        //                return "NULL";
+    //        //            }
+    //        //            return "b'" + TextUtil.byteToBitStr(bytes) + "'";
+    //        //        }
+    //        //        if (column.supportEnum()) {
+    //        //            return "'" + value + "'";
+    //        //        }
+    //        //        if (column.supportString()) {
+    //        //            String str = TextUtil.escape((String) value);
+    //        //            return "\"" + str + "\"";
+    //        //        }
+    //        //        return value;
+    //        if (column.supportGeometry()) {
+    //            return "ST_GeomFromText('" + value + "')";
+    //        }
+    //        return DBUtil.wrapData(value, dialect);
+    //    }
+
     /**
      * 参数化，sql
      *
@@ -174,54 +239,37 @@ public class DBDataUtil {
      * @return 参数化后的值
      */
     public static Object parameterizedForSql(DBColumn column, Object value, DBDialect dialect) {
-        //        if (value == null) {
-        //            return "NULL";
-        //        }
-        //        if (column.supportGeometry()) {
-        //            return "ST_GeomFromText('" + value + "')";
-        //        }
-        //        if (column.isDateType()) {
-        //            Date date = (Date) value;
-        //            return "'" + DateHelper.formatDate(date) + "'";
-        //        }
-        //        if (column.supportTimestamp()) {
-        //            return "'" + value + "'";
-        //        }
-        //        if (column.supportJson()) {
-        //            return "'" + value + "'";
-        //        }
-        //        if (column.supportBinary()) {
-        //            byte[] bytes = (byte[]) value;
-        //            if (bytes.length == 0) {
-        //                return "NULL";
-        //            }
-        //            return "0x" + HexUtil.encodeHexStr(bytes, false);
-        //        }
-        //        if (column.supportBoolean()) {
-        //            if (value instanceof Boolean b) {
-        //                return b ? "1" : "0";
-        //            }
-        //        }
-        //        if (column.supportBit()) {
-        //            if (value instanceof Boolean b) {
-        //                return b ? "1" : "0";
-        //            }
-        //            byte[] bytes = (byte[]) value;
-        //            if (bytes.length == 0) {
-        //                return "NULL";
-        //            }
-        //            return "b'" + TextUtil.byteToBitStr(bytes) + "'";
-        //        }
-        //        if (column.supportEnum()) {
-        //            return "'" + value + "'";
-        //        }
-        //        if (column.supportString()) {
-        //            String str = TextUtil.escape((String) value);
-        //            return "\"" + str + "\"";
-        //        }
-        //        return value;
+        if (value == null) {
+            return "NULL";
+        }
         if (column.supportGeometry()) {
             return "ST_GeomFromText('" + value + "')";
+        }
+        if (column.isDateType()) {
+            Date date = (Date) value;
+            value = DateHelper.formatDate(date);
+        }
+        if (column.supportBinary()) {
+            byte[] bytes = (byte[]) value;
+            if (bytes.length == 0) {
+                return "NULL";
+            }
+            return "0x" + HexUtil.encodeHexStr(bytes, false);
+        }
+        if (column.supportBoolean()) {
+            if (value instanceof Boolean b) {
+                return b ? "1" : "0";
+            }
+        }
+        if (column.supportBit()) {
+            byte[] bytes = (byte[]) value;
+            if (bytes.length == 0) {
+                return "NULL";
+            }
+            return "b'" + TextUtil.byteToBitStr(bytes) + "'";
+        }
+        if (column.supportString()) {
+            value = TextUtil.escape((String) value);
         }
         return DBUtil.wrapData(value, dialect);
     }
