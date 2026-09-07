1d3de6fd88f8f97184ed107cdd4899a79a7e66b9
oyzh <oyzh.1994@q.qcom>
2026-09-07 10:42:23 +0800

    update: 完善覆盖检查

--- fx-plus/src/main/java/cn/oyzh/fx/plus/util/FXCoverChecker.java
+++ fx-plus/src/main/java/cn/oyzh/fx/plus/util/FXCoverChecker.java
@@ -1,8 +1,8 @@
 package cn.oyzh.fx.plus.util;
 
 import cn.oyzh.common.log.JulLog;
-import cn.oyzh.common.system.OSUtil;
 import cn.oyzh.common.util.ClassUtil;
+import cn.oyzh.common.util.CoverUtil;
 import cn.oyzh.fx.gui.tabs.RichTab;
 import cn.oyzh.fx.plus.window.PopupAdapter;
 import cn.oyzh.fx.plus.window.PopupAttribute;
@@ -23,16 +23,16 @@ import java.util.function.Predicate;
  * @author oyzh
  * @since 2026/09/06
  */
-public class CoverManager {
+public class FXCoverChecker {
 
-    private static String projectPath;
+    private String projectPath;
 
-    public static String getProjectPath() {
+    public String getProjectPath() {
         return projectPath;
     }
 
-    public static void setProjectPath(String projectPath) {
-        CoverManager.projectPath = projectPath;
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
     }
 
     /**
@@ -41,22 +41,9 @@ public class CoverManager {
      * @return 结果
      * @throws Exception 异常
      */
-    public static List<Class<?>> getClasses() throws Exception {
+    public List<Class<?>> getClasses() throws Exception {
         String pkName = "";
-        String f = projectPath;
-        if (projectPath.contains("test-classes")) {
-            f = projectPath.substring(0, f.indexOf("test-classes"));
-        } else if (projectPath.contains("classes")) {
-            f = projectPath.substring(0, f.indexOf("classes"));
-        }
-        if (f.startsWith("file:/")) {
-            if (OSUtil.isWindows()) {
-                f = f.substring(6);
-            } else {
-                f = f.substring(5);
-            }
-        }
-        f += "classes";
+        String f = CoverUtil.getClassesPath(this.projectPath);
         List<Class<?>> list = new ArrayList<>();
         ClassUtil.findClassesInDirectory(new File(f), pkName, list, (Predicate<Class<?>>) aClass -> {
             if (Modifier.isAbstract(aClass.getModifiers()) || !Modifier.isPublic(aClass.getModifiers())) {
@@ -73,9 +60,9 @@ public class CoverManager {
      *
      * @throws Exception 异常
      */
-    public static void tabCheck() throws Exception {
+    public void tabCheck() throws Exception {
         JulLog.info("tab check start");
-        for (Class<?> aClass : getClasses()) {
+        for (Class<?> aClass : this.getClasses()) {
             if (RichTab.class.isAssignableFrom(aClass)) {
                 RichTab tab = (RichTab) ClassUtil.newInstance(aClass);
                 if (tab == null) {
@@ -92,9 +79,9 @@ public class CoverManager {
      *
      * @throws Exception 异常
      */
-    public static void viewCheck() throws Exception {
+    public void viewCheck() throws Exception {
         JulLog.info("stage check start");
-        for (Class<?> aClass : getClasses()) {
+        for (Class<?> aClass : this.getClasses()) {
             StageAttribute attribute = aClass.getAnnotation(StageAttribute.class);
             if (attribute == null) {
                 continue;
@@ -113,9 +100,9 @@ public class CoverManager {
      *
      * @throws Exception 异常
      */
-    public static void popupCheck() throws Exception {
+    public void popupCheck() throws Exception {
         JulLog.info("popup check start");
-        for (Class<?> aClass : getClasses()) {
+        for (Class<?> aClass : this.getClasses()) {
             PopupAttribute attribute = aClass.getAnnotation(PopupAttribute.class);
             if (attribute == null) {
                 continue;
